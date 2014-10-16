757f11a8b44441800d2f56891d082c08085f1e09
Mynor Alexander <sinrart@yahoo.com>
2014-10-15 23:21:48 -0700

    new build includes partials views and controller. Working with  service. Added new list && detail HTML section. Added navigation to details section to cycle through elements within the array. Simeple CSS transitions added but used angular animations to further enhance the final build

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,29 @@
-// namespace
-var myApp = angular.module("myApp", []);
+var artistControllers = angular.module('artistControllers', ['ngAnimate']);
 
-myApp.controller("MyController", ["$scope", "$http", function ($scope, $http) {
-	$http.get("js/data.json").success( function (data){
+artistControllers.controller('ListController', ['$scope', '$http', function ($scope, $http) {
+	$http.get('js/data.json').success(function (data){
 		$scope.artists = data;
 		$scope.artistOrder = 'name';
 	});
-}]);
+}]); // artistControllers.controller ListController
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+	$http.get('js/data.json').success(function (data){
+		$scope.artists = data;
+		$scope.whichItem = $routeParams.itemId;
+
+	    if ($routeParams.itemId > 0) {
+	      $scope.prevItem = Number($routeParams.itemId)-1;
+	    } else {
+	      $scope.prevItem = $scope.artists.length-1;
+	    }
+
+	    if ($routeParams.itemId < $scope.artists.length-1) {
+	      $scope.nextItem = Number($routeParams.itemId)+1;
+	    } else {
+	      $scope.nextItem = 0;
+	    }
+
+	});
+
+}]); // artistControllers.controller DetailsController
